f371a783582d9112da5d86bfe86b9264dab27609
Rako <tsiory.r17@gmail.con>
2025-01-02 16:21:22 -0500

    adding the about route

--- shop/models.py
+++ shop/models.py
@@ -28,6 +28,10 @@ class Product(models.Model):
     description = models.CharField(max_length=350 , default='' , blank= True)
     image = models.ImageField(upload_to='uploads/product')
     
+    #add sale
+    is_sale = models.BooleanField(default=False)
+    sale_price = models.DecimalField(decimal_places=2,max_digits=7,default=0)
+    
     def __str__(self):
       return self.name
     
--- shop/urls.py
+++ shop/urls.py
@@ -3,4 +3,5 @@ from django.urls import path , include
 from . import views
 urlpatterns = [
     path('', views.home, name ='home'),
+    path('about/',views.about , name = 'about')
 ]
