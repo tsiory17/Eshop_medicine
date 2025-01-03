9bd88523e92e358beb47fd3f5d5b10295df7da65
Rako <tsiory.r17@gmail.con>
2025-01-03 18:51:13 -0500

    add details view and login function

--- shop/urls.py
+++ shop/urls.py
@@ -3,5 +3,10 @@ from django.urls import path , include
 from . import views
 urlpatterns = [
     path('', views.home, name ='home'),
-    path('about/',views.about , name = 'about')
+    path('about/',views.about , name = 'about'),
+    path('product/<int:pk>',views.product , name = 'product'),
+    path('login/', views.login_customer, name ='login'),
+    path('logout/', views.logout_customer, name ='logout'),
+
+
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,8 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Product
+from django.contrib.auth import authenticate,login,logout
+from django.contrib import messages
+
+
 def home (request):
   products = Product.objects.all()
   return render(request , 'home.html' , {'products': products})
 
 def about(request):
   return render(request,'about.html',{})
+
+def product (request, pk):
+  product = Product.objects.get(id = pk)
+  return render (request,'product.html', {'product':product})
+
+def login_customer (request):
+  if request.method == "POST":
+    username = request.POST['username']
+    password = request.POST['password']
+    
+    user = authenticate(request , username=username , password = password)
+    if user is not None:
+      login(request,user)
+      messages.success(request,"You are logged in")
+     
+      return redirect('home')
+    else:
+      messages.error(request,"Username or Password invalid please try again")
+      return render(request,'login.html',{})
+  else:
+      return render(request,'login.html',{})
+
+
+def logout_customer (request):
+  logout(request)
+  messages.success(request, "You have logged out successfully")
+  return redirect ('home')
