52ac9ad87e6e74d9a24693865089d2f03f2576fe
Rako <tsiory.r17@gmail.con>
2025-01-04 17:17:25 -0500

    registration template

--- shop/views.py
+++ shop/views.py
@@ -42,3 +42,21 @@ def logout_customer (request):
   messages.success(request, "You have logged out successfully")
   return redirect ('home')
 
+def register_user (request):
+  form = SignUpForm()
+  if request.method =="POST":
+    form = SignUpForm(request.POST)
+    if form.is_valid():
+      form.save()
+      username = form.cleaned_data['username']
+      password = form.cleaned_data['password1']
+      
+      user = authenticate(username=username , password = password)
+      login(request,user)
+      messages.success(request,"Registration successful")
+      return redirect('home')
+    else:
+      messages.error(request, "Registration failed , please try again")
+      return redirect('register')
+  else:
+     return render(request,'register.html',{})
